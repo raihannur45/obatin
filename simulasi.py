eb7270baa5dc820336590aa6bc945d88589075ba
Raihan Nur Ramadhan <96563545+raihannur45@users.noreply.github.com>
2022-06-01 12:40:41 +0700

    file dari path machine learning

---
+++ simulasi.py
@@ -0,0 +1,107 @@
+#import library yang digunakan
+from flask import Flask, request, jsonify
+import string
+from chatbot_model import*
+
+#memuat model yang telah dibuat sebelumnya
+new_model = tf.keras.models.load_model('saved_model/my_model')
+new_model.summary()
+loss, acc = new_model.evaluate(testing_padded, label_test_np)
+
+#memuat list yang berisi nama-nama obat di pasaran ke program
+list_namaobat = []
+list_namaobat_lower = []
+with open('nama-obat.txt') as txtnamaobat:
+  for namaobat in txtnamaobat:
+    namaobat = namaobat.strip()
+    namaobatlower = namaobat.lower()
+    list_namaobat.append(namaobat)
+    list_namaobat_lower.append(namaobatlower)
+
+#memuat file yang berisi respon dari bot untuk setiap label
+responses = {}
+tag = ''
+with open('data_respon.csv') as csvdata:
+  readcsv = csv.reader(csvdata, delimiter=',')
+  for row in readcsv:
+    if tag == row[0]:
+      responses[row[0]].append(row[1])
+    else:
+      newlist = []
+      newlist.append(row[1])
+      responses[row[0]] = newlist
+
+    tag = row[0]
+
+#memuat file yang berisi database obat yang dimiliki
+listobat = {}
+with open('data_obat.csv') as csvdata:
+  readcsv = csv.reader(csvdata, delimiter=';')
+  for row in readcsv:
+    newdict = {}
+    newdict['kandungan'] = row[1]
+    newdict['indikasi'] = row[2]
+    newdict['efeksamping'] = row[3]
+    newdict['cara'] = row[4]
+    row[0] = row[0].lower()
+    listobat[row[0]] = newdict
+
+#label-label yang digunakan untuk mencari data di database obat
+search = ['efeksamping', 'cara', 'kandungan', 'indikasi']
+
+#inisialisasi Flask
+app = Flask(__name__)
+
+#endpoint untuk memprediksi input user
+@app.route("/bot")
+def chatbot():
+  nama_obat = ''
+  search_database = 0
+  input_user = []
+
+  user_input = request.args.get('text')
+  user_input = user_input.lower()
+  for char in user_input:
+    if char in string.punctuation:
+        user_input = user_input.replace(char, "")
+  user_input = stemmer.stem(user_input)
+  input_user.append(user_input)
+
+  user_input_token = tokenizer.texts_to_sequences(input_user)
+  user_input_token = pad_sequences(user_input_token, maxlen=MAXLEN, padding='post')
+
+  predik = new_model.predict(user_input_token)
+  output = predik.argmax() + 1
+  for k,v in word_index.items():
+    if v == output:
+      label_output = k
+
+  if label_output in search:
+    for obat in list_namaobat_lower:
+      if obat in user_input:
+        nama_obat = obat
+        search_database = 1
+        break
+
+  return jsonify(label = label_output, namaobat = nama_obat, status = str(search_database))
+
+#endpoint untuk mencari data di database obat yang dimiliki
+@app.route("/database")
+def search_database():
+  req_label = request.args.get('label')
+  req_nama_obat = request.args.get('namaobat')
+  if req_nama_obat in listobat:
+    jawaban = listobat[req_nama_obat][req_label]
+  else:
+    jawaban = 'Data obat tidak ditemukan'
+  return jsonify(jawab = jawaban)
+
+#endpoint untuk mencari respon bot sesuai label yang diprediksi
+@app.route("/respon")
+def respon_bot():
+  req_label = request.args.get('label')
+  jawaban = random.choice(responses[req_label])
+  return jsonify(jawab = jawaban)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
